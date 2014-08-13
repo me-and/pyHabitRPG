5d54867e6e69be537a4d017ea7e3b24df9b952fe
Adam Dinwoodie <adam@dinwoodie.org>
2014-08-13 15:56:41 +0100

    Let the main module look after login details
    The main habitrpg module now looks after loading login details, so don't
    try to do it in this script.

--- overtasked.py
+++ overtasked.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-import os.path
-
 import habitrpg
 
-LOGIN_DETAIL_FILE = '~/.habitrpg'
 MAX_TODOS = 20
 CLEAR_THRESHOLD = 15
 TASK_NAME = 'Cut the todo list down to ≤{} tasks'.format(CLEAR_THRESHOLD)
 
 if __name__ == '__main__':
-    with open(os.path.expanduser(LOGIN_DETAIL_FILE)) as login_file:
-        user_id = login_file.readline().strip()
-        api_token = login_file.readline().strip()
-
-    hrpg = habitrpg.HabitRPG(user_id, api_token)
+    hrpg = habitrpg.HabitRPG.login_from_file()
     tasks = hrpg.tasks()
 
     incomplete_todos = sum(1 for task in tasks if
