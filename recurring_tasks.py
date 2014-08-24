4700b9f8b9ece6d3d923faac73e4c71a2b24a9f9
Adam Dinwoodie <adam@dinwoodie.org>
2014-08-24 22:50:46 +0100

    Add script for managing recurring tasks
    It's pretty basic at the moment, but this script will look after
    creating a new task some time after a previous task has completed.
    
    It stores data about the tasks in YAML files in a hard-coded
    subdirectory of the user's home directory; the idea being that users can
    edit these as necessary, and eventually will be able to write them to
    create new recurring tasks, but for the program to also be able to
    update them easily.
    
    Currently the easiest way to create a new recurring task is to use a
    function available in the script; best way to access that is to import
    the script in the REPL.

---
+++ recurring_tasks.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import os
+import datetime
+from random import randint
+from tempfile import mkstemp
+
+import yaml
+from pytz import timezone
+
+import habitrpg
+
+SECONDS_PER_DAY = 60 * 60 * 24
+TASK_DIRECTORY = os.path.expanduser(os.path.join('~', '.habitrpg_tasks'))
+TZ = timezone('Europe/London')
+
+# Not supposed to be used as part of the main script; this is here as a
+# convenience function until the code can cope without needing tasks to be
+# bootstrapped.
+def create_recurring_task(text, filename, min, max):
+    task = habitrpg.Todo.create(habitrpg.HabitRPG.login_from_file(), text=text)
+    task_data = {'current': {'created': task.date_created,
+                             'id': task.id_code},
+                 'next': None,
+                 'previous': None,
+                 'repeat': {'min': min, 'max': max},
+                 'text': text}
+    file_path = os.path.join(TASK_DIRECTORY, filename)
+    with open(file_path, 'w') as task_file:
+        yaml.safe_dump(task_data, task_file, default_flow_style=False)
+
+if __name__ == '__main__':
+    hrpg = habitrpg.HabitRPG.login_from_file()
+    for filename in os.listdir(TASK_DIRECTORY):
+        file_path = os.path.join(TASK_DIRECTORY, filename)
+        with open(file_path) as task_file:
+            task_data = yaml.safe_load(task_file)
+
+        # Fix up any timestamps, because YAML's loader automatically converts
+        # them to UTC and removes any timezone information (including the fact
+        # that they're now UTC), meaning datetime comparisons don't work
+        # against timestamps from the HabitRPG API, which do include timezone
+        # information.
+        #
+        # Do this even for timestamps we don't use, because YAML's dumper does
+        # at least preserve the UTC offset if it's stored in the datetime
+        # object, so we might as well keep that information handy.
+        try:
+            task_data['current']['created'] = (task_data['current']['created']
+                    .replace(tzinfo=datetime.timezone.utc))
+        except TypeError:  # task_data['current'] == None
+            pass
+        try:
+            task_data['next'] = (task_data['next']
+                    .replace(tzinfo=datetime.timezone.utc))
+        except AttributeError:  # task_data['next'] == None
+            pass
+        try:
+            task_data['previous']['created'] = (task_data['previous']
+                    ['created'].replace(tzinfo=datetime.timezone.utc))
+            task_data['previous']['completed'] = (task_data['previous']
+                    ['completed'].replace(tzinfo=datetime.timezone.utc))
+        except TypeError:  # task_data['previous'] == None
+            pass
+
+        if task_data['current'] is not None:
+            task = habitrpg.Todo.get(hrpg, task_data['current']['id'])
+            if task.completed:
+                print('Task "{}" was completed!'.format(task_data['text']))
+                task_data['previous'] = task_data['current']
+                task_data['previous']['completed'] = task.date_completed
+                task_data['current'] = None
+                min_seconds = task_data['repeat']['min'] * SECONDS_PER_DAY
+                max_seconds = task_data['repeat']['max'] * SECONDS_PER_DAY
+                task_data['next'] = (task.date_completed +
+                        datetime.timedelta(seconds=randint(min_seconds,
+                                                           max_seconds)))
+
+        if (task_data['next'] is not None and
+                datetime.datetime.now(TZ) >= task_data['next']):
+            task = habitrpg.Todo.create(hrpg, text=task_data['text'])
+            task_data['current'] = {'id': task.id_code,
+                                    'created': task.date_created}
+            task_data['next'] = None
+
+        # Write to a temporary file then move it into place, else something
+        # going wrong while writing the file will clobber the old data there
+        # too.
+        temp_handle, temp_path = mkstemp(suffix='.tmp', prefix='.',
+                                         dir=TASK_DIRECTORY)
+        with os.fdopen(temp_handle, 'w') as task_file:
+            yaml.safe_dump(task_data, task_file, default_flow_style=False)
+        os.rename(temp_path, file_path)
