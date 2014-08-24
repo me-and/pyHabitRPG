16c32e1c0c05e96ab07de801994656aa7974bfd2
Adam Dinwoodie <adam@dinwoodie.org>
2014-08-24 22:40:27 +0100

    Get the right call for creating a Todo
    The Todo class is in the habitrpg namespace; calling it bare doesn't
    work.  The call also now needs to specify the HabitRPG instance.

--- overtasked.py
+++ overtasked.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
                        None)
 
     if incomplete_todos > MAX_TODOS and reduce_task is None:
-        Todo.create(text=TASK_NAME)
+        habitrpg.Todo.create(hrpg, text=TASK_NAME)
     if incomplete_todos <= (CLEAR_THRESHOLD + 1) and reduce_task is not None:
         reduce_task.complete()
