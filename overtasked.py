686f44fecee742d501ed99be961b14be189c63c4
Adam Dinwoodie <adam@dinwoodie.org>
2015-08-04 02:20:54 +0100

    Break apart function code
    Rather than doing almost everything in the body of the module, break
    most of the work for `overtasked.py` out into separate functions.

--- overtasked.py
+++ overtasked.py
@@ -11,20 +11,18 @@ CLEAR_THRESHOLD = 18
 TASK_NAME = 'Cut the todo list down to ≤{} tasks'.format(CLEAR_THRESHOLD)
 TZ = timezone('Europe/London')
 
-if __name__ == '__main__':
-    user = habitrpg.User.from_file()
-    user.fetch_tasks()
+def find_incomplete_todos(todos):
+    return [todo for todo in todos if not todo.completed]
 
-    reduce_task = None
-    num_todos = 0
-    for task in user.todos:
-        if not task.completed:
-            num_todos += 1
-        if task.title == TASK_NAME and not task.completed:
-            reduce_task = task
+def find_reduce_task(todos):
+    try:
+        return next((todo for todo in todos if todo.title == TASK_NAME))
+    except StopIteration:  # No such task
+        return None
 
+def create_update_reduce_task(reduce_task, num_todos, print_count=True):
     notes = '{:%A %I:%M %p}: {} tasks'.format(datetime.now(TZ), num_todos)
-    if sys.stdout.isatty():
+    if print_count and sys.stdout.isatty():
         print(notes)
 
     if num_todos > MAX_TODOS and reduce_task is None:
@@ -33,3 +31,12 @@ if __name__ == '__main__':
         reduce_task.complete()
     elif reduce_task is not None:
         reduce_task.update(notes=notes)
+
+if __name__ == '__main__':
+    user = habitrpg.User.from_file()
+    user.fetch_tasks()
+
+    incomplete_todos = find_incomplete_todos(user.todos)
+    reduce_task = find_reduce_task(incomplete_todos)
+
+    create_update_reduce_task(reduce_task, len(incomplete_todos))
